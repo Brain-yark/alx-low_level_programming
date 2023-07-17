59192268c6c7f54353740224c6423fc47839c66e
MegaMind <esamba333@gmail.com>
2023-07-17 16:57:23 -0700

    my quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<unistd.h>
+/**
+ *make- Entry points
+ * 
+ *Return: always 0 (succesfull)
+ *
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 57);
+	return (1);
+}
+
