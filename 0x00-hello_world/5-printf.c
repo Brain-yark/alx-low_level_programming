8ea7eaee7086cce0a42ec7346193913061a05036
MegaMind <esamba333@gmail.com>
2023-07-17 13:25:41 -0700

    print f function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *make- Entry points
+ * 
+ *Return: always 0 (succesfull)
+ *
+ **/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
