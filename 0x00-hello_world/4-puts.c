e553ad9e02c2fa0d53dc6943f2c375d83bf06d61
MegaMind <esamba333@gmail.com>
2023-07-17 12:58:51 -0700

    4-puts file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * make- Entry points
+ * 
+ * return always 0 (succesfull)
+ *
+ **/
+int main(void)
+{
+	puts("programming is like building multilingual puzzle\n");
+	return(0);
+}
